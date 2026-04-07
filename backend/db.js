888bc7f8cd51c7ca73f0a19ef1086a4ea83ad840
ShivanshuSharmaLPU <shivanshusharma8398@gmail.com>
2026-04-07 23:16:59 +0530

    Fix SQLite path for Render deployment

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const DB_PATH = path.join(__dirname, 'database.sqlite');
+const DB_PATH = '/tmp/database.sqlite';
 
 const db = new sqlite3.Database(DB_PATH, (err) => {
   if (err) {
