ae6b1656c724602d05c1a0448f065bf9961df273
ShivanshuSharmaLPU <shivanshusharma8398@gmail.com>
2026-04-07 22:56:59 +0530

    Connected frontend to Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({
+  baseURL: process.env.BACKEND_URL
+});
 
 // Projects
 export const getProjects = (page = 1, limit = 10) =>
